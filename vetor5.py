bd1c548ffd63a4eda56efd44af828d9d33d8cd4a
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:48:21 -0300

    Update vetor5.py

--- vetor5.py
+++ vetor5.py
@@ -1,22 +1,30 @@
 from prettytable import PrettyTable
 
-vetor = []
+def ler_valores():
+    vetor = []
+    for i in range(10):
+        valor = int(input(f'Digite o valor {i+1}: '))
+        vetor.append(valor)
+    return vetor
 
-for i in range(10):
-    valor = int(input(f'Digite o valor {i+1}: '))
-    vetor.append(valor)
+def contar_valores_pares(vetor):
+    contador = 0
+    for valor in vetor:
+        if valor % 2 == 0:
+            contador += 1
+    return contador
 
-contador = 0
-for valor in vetor:
-    if valor % 2 == 0:
-        contador += 1
+def exibir_tabela(vetor):
+    tabela = PrettyTable()
+    tabela.field_names = ['Vetor']
 
-tabela = PrettyTable()
-tabela.field_names = ['Vetor']
+    for valor in vetor:
+        tabela.add_row([valor])
 
-for valor in vetor:
-    tabela.add_row([valor])
+    print('Valores do vetor:')
+    print(tabela)
 
-print('Valores do vetor:')
-print(tabela)
+vetor = ler_valores()
+contador = contar_valores_pares(vetor)
+exibir_tabela(vetor)
 print(f'O vetor possui {contador} valores pares.')
