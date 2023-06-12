d689e9af01b133dade5b9afb230ece5df72373d6
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:44:49 -0300

    Update vetor2.py

--- vetor2.py
+++ vetor2.py
@@ -1,14 +1,18 @@
 from prettytable import PrettyTable
 
-valores = []
+def ler_valores():
+    valores = []
+    for i in range(6):
+        valor = int(input(f'Digite o valor {i + 1}: '))
+        valores.append(valor)
+    return valores
 
-for i in range(6):
-    valor = int(input(f'Digite o valor {i + 1}: '))
-    valores.append(valor)
+def exibir_tabela(valores):
+    tabela = PrettyTable()
+    tabela.field_names = ['Valores lidos:']
+    for valor in reversed(valores):
+        tabela.add_row([valor])
+    print(tabela)
 
-tabela = PrettyTable()
-tabela.field_names = ['Valores lidos:']
-
-for valor in valores:
-    tabela.add_row([valor])
-print(tabela)
+valores = ler_valores()
+exibir_tabela(valores)
