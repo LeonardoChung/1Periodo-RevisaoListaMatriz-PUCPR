5a22695ee98586760b59ac286598bf69cbe59d43
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:40:43 -0300

    Update matriz11.py

--- matriz11.py
+++ matriz11.py
@@ -1,23 +1,30 @@
 from prettytable import PrettyTable
 
-matriz = []
-for i in range(3):
-    linha = []
-    for j in range(3):
-        valor = int(input('Digite o valor da posição [{0}][{1}]: '.format(i+1, j+1)))
-        linha.append(valor)
-    matriz.append(linha)
+def ler_matriz():
+    matriz = []
+    for i in range(3):
+        linha = []
+        for j in range(3):
+            valor = int(input('Digite o valor da posição [{0}][{1}]: '.format(i+1, j+1)))
+            linha.append(valor)
+        matriz.append(linha)
+    return matriz
 
-soma = 0
-for i in range(3):
-    soma += matriz[i][2-i]
+def calcular_soma_diagonal_secundaria(matriz):
+    soma = 0
+    for i in range(3):
+        soma += matriz[i][2-i]
+    return soma
 
-tabela = PrettyTable()
-tabela.field_names = ['Coluna {0}'.format(j+1) for j in range(3)]
+def exibir_matriz(matriz):
+    tabela = PrettyTable()
+    tabela.field_names = ['Coluna {0}'.format(j+1) for j in range(3)]
+    for linha in matriz:
+        tabela.add_row(linha)
+    print('Matriz:')
+    print(tabela)
 
-for linha in matriz:
-    tabela.add_row(linha)
-
-print('Matriz:')
-print(tabela)
-print('Soma da diagonal secundária: {0}'.format(soma))
+matriz = ler_matriz()
+soma_diagonal_secundaria = calcular_soma_diagonal_secundaria(matriz)
+exibir_matriz(matriz)
+print('Soma da diagonal secundária: {0}'.format(soma_diagonal_secundaria))
