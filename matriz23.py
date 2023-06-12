f66857774a44b89eb77bc3cf70537153fcaa7efb
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:41:28 -0300

    Update matriz23.py

--- matriz23.py
+++ matriz23.py
@@ -1,26 +1,33 @@
 from prettytable import PrettyTable
 
-A = []
-for i in range(3):
-    linha = []
-    for j in range(3):
-        valor = int(input(f'Digite o valor da posição [{i+1}][{j+1}]: '))
-        linha.append(valor)
-    A.append(linha)
+def ler_matriz():
+    matriz = []
+    for i in range(3):
+        linha = []
+        for j in range(3):
+            valor = int(input(f'Digite o valor da posição [{i+1}][{j+1}]: '))
+            linha.append(valor)
+        matriz.append(linha)
+    return matriz
 
-B = []
-for i in range(3):
-    linha = []
-    for j in range(3):
-        elemento = A[i][j] ** 2
-        linha.append(elemento)
-    B.append(linha)
+def calcular_quadrado_matriz(matriz):
+    matriz_quadrado = []
+    for i in range(3):
+        linha = []
+        for j in range(3):
+            elemento = matriz[i][j] ** 2
+            linha.append(elemento)
+        matriz_quadrado.append(linha)
+    return matriz_quadrado
 
-tabela = PrettyTable()
-tabela.field_names = [f'Coluna {j+1}' for j in range(3)]
+def exibir_matriz(matriz, nome):
+    tabela = PrettyTable()
+    tabela.field_names = [f'Coluna {j+1}' for j in range(3)]
+    for linha in matriz:
+        tabela.add_row(linha)
+    print(f'Matriz {nome}:')
+    print(tabela)
 
-for linha in B:
-    tabela.add_row(linha)
-
-print('Matriz B = A^2:')
-print(tabela)
+matriz_a = ler_matriz()
+matriz_b = calcular_quadrado_matriz(matriz_a)
+exibir_matriz(matriz_b, 'B = A^2')
