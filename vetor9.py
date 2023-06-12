5e52c34c4aa58ce39cb4d7cb1aa8c2d68b373198
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:49:03 -0300

    Update vetor9.py

--- vetor9.py
+++ vetor9.py
@@ -1,19 +1,22 @@
 from prettytable import PrettyTable
 
-valores = []
-
-for i in range(6):
-    valor = int(input('Digite um valor inteiro par: '))
-    while valor % 2 != 0:
+def ler_valores():
+    valores = []
+    for i in range(6):
         valor = int(input('Digite um valor inteiro par: '))
-    valores.append(valor)
+        while valor % 2 != 0:
+            valor = int(input('Digite um valor inteiro par: '))
+        valores.append(valor)
+    return valores
 
-tabela = PrettyTable()
-tabela.field_names = ['Valores lidos na ordem inversa']
+def exibir_tabela(valores):
+    tabela = PrettyTable()
+    tabela.field_names = ['Valores lidos na ordem inversa']
 
-for valor in reversed(valores):
-    tabela.add_row([valor])
+    for valor in reversed(valores):
+        tabela.add_row([valor])
 
-tabela.reversesort = True
+    print(tabela)
 
-print(tabela)
+valores = ler_valores()
+exibir_tabela(valores)
