8524d3fe51cda17b3f9cc1693bf6caa2e8ce513f
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:42:31 -0300

    Update vetor1.py

--- vetor1.py
+++ vetor1.py
@@ -1,16 +1,27 @@
 from prettytable import PrettyTable
+
+def calcular_soma(valores):
+    soma = valores[0] + valores[1] + valores[-1]
+    return soma
+
+def substituir_valor(valores, indice, novo_valor):
+    valores[indice] = novo_valor
+    return valores
+
+def exibir_tabela(valores):
+    tabela = PrettyTable()
+    tabela.field_names = ['Valores']
+    for valor in valores:
+        tabela.add_row([valor])
+    print(tabela)
+
 a = [1, 0, 5, -2, -5, 7]
 
-soma = a[0] + a[1] + a[5]
+soma = calcular_soma(a)
 print('-' * 65)
-print('A soma entre o primeiro, o segundo e ultimo valor é igual a {}.'.format(soma))
+print('A soma entre o primeiro, o segundo e o último valor é igual a {}.'.format(soma))
 print('-' * 65)
 
-a[4] = 100
-tabela = PrettyTable()
-tabela.field_names = ['Valores']
+a = substituir_valor(a, 4, 100)
 
-for valor in a:
-    tabela.add_row([valor])
-
-print(tabela)
+exibir_tabela(a)
