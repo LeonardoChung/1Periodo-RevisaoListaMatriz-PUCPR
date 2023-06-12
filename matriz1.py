f40d57f75a3ebde44ba3d52918343ff1187193f8
LeonardoChung <127551335+LeonardoChung@users.noreply.github.com>
2023-06-11 22:39:48 -0300

    Update matriz1.py

--- matriz1.py
+++ matriz1.py
@@ -1,25 +1,32 @@
 from prettytable import PrettyTable
 
-matriz = []
-for i in range(4):
-    linha = []
-    for j in range(4):
-        valor = int(input(f'Digite o valor da posição [{i+1}][{j+1}]: '))
-        linha.append(valor)
-    matriz.append(linha)
+def ler_matriz():
+    matriz = []
+    for i in range(4):
+        linha = []
+        for j in range(4):
+            valor = int(input(f'Digite o valor da posição [{i+1}][{j+1}]: '))
+            linha.append(valor)
+        matriz.append(linha)
+    return matriz
 
-contador = 0
-for linha in matriz:
-    for valor in linha:
-        if valor > 10:
-            contador += 1
+def contar_valores_maiores(matriz):
+    contador = 0
+    for linha in matriz:
+        for valor in linha:
+            if valor > 10:
+                contador += 1
+    return contador
 
-tabela = PrettyTable()
-tabela.field_names = [f'Coluna {j+1}' for j in range(4)]
+def exibir_matriz(matriz):
+    tabela = PrettyTable()
+    tabela.field_names = [f'Coluna {j+1}' for j in range(4)]
+    for linha in matriz:
+        tabela.add_row(linha)
+    print('Matriz:')
+    print(tabela)
 
-for linha in matriz:
-    tabela.add_row(linha)
-
-print('Matriz:')
-print(tabela)
+matriz = ler_matriz()
+contador = contar_valores_maiores(matriz)
+exibir_matriz(matriz)
 print(f'A matriz possui {contador} valores maiores que 10.')
